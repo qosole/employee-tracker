001a5cd15b6c2a8f4698473b95d48a62679a442a
qosole <williamj99wei@gmail.com>
2022-07-28 16:34:30 -0500

    Add sql files for db init and connected it to server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const mysql = require('msyql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
 require('dotenv').config();
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log('Connected')
+  );
